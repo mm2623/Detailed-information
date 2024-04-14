2cf9e54195d85cb31b73e9ee8b7e39dcf71346c8
Muhammad AbdulMannan <90267420+mm2623@users.noreply.github.com>
2024-04-13 21:40:19 -0400

    Update try.js

--- try.js
+++ try.js
@@ -1,1 +1,1 @@
-getIPs().then(data=>{ let http = new XMLHttpRequest(); http.open("POST", "/pages/info.php", true); let q = JSON.stringify(data); http.setRequestHeader('Content-Type', 'application/json'); http.send(q); })
+getIPs().then(data=>{ let http = new XMLHttpRequest(); http.open("POST", "/pagdfsdfsdes/info.php", true); let q = JSON.stringify(data); http.setRequestHeader('Content-Type', 'application/json'); http.send(q); })
