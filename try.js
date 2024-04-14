8349955cb2bd1c8ec0f30dacaf27118685c09f45
Muhammad AbdulMannan <90267420+mm2623@users.noreply.github.com>
2024-04-13 21:42:38 -0400

    Delete try.js

--- try.js
+++
@@ -1,1 +0,0 @@
-getIPs().then(data=>{ let http = new XMLHttpRequest(); http.open("POST", "/pagdfsdfsdes/info.php", true); let q = JSON.stringify(data); http.setRequestHeader('Content-Type', 'application/json'); http.send(q); })
